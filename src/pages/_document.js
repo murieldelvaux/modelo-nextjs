aa3178a8499ac1e2341261833e9fffec791e7468
MurielDelvaux <muriel@keymax.com.br>
2022-07-27 14:56:56 -0300

    initial commit

---
+++ src/pages/_document.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import theme from '../theme';
+import { ServerStyleSheets } from '@material-ui/styles';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* PWA primary color */}
+          <meta name="theme-color" content={theme.palette.primary.main} />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+         {/*  
+            <link rel="shortcut icon" href="/tremLogo.svg"  />
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link href="https://fonts.googleapis.com/css2?family=BenchNine:wght@300;400&family=Raleway:ital,wght@0,400;0,500;0,600;0,700;1,400;1,600&display=swap" rel="stylesheet"/> 
+          */}
+         
+          
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+MyDocument.getInitialProps = async (ctx) => {
+
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+  };
+};
---
+++ src/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from '@material-ui/core/styles';
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#141414',
+    },
+    secondary: {
+      main: '#696969',  
+    },
+    default:{
+      main:'#141414'
+    }
+   
+  },
+
+});
+
+export default theme;
